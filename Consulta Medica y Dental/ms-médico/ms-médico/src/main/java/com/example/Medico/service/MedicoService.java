994a67016796491df257dd21be259f418d8ff02f
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-05-01 19:00:27 -0400

    feat:update service

--- Consulta Medica y Dental/ms-médico/ms-médico/src/main/java/com/example/Medico/service/MedicoService.java
+++ Consulta Medica y Dental/ms-médico/ms-médico/src/main/java/com/example/Medico/service/MedicoService.java
@@ -1,8 +1,6 @@
 package com.example.Medico.service;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.Medico.model.Medico;
@@ -16,5 +14,15 @@ public class MedicoService {
         return medicoRepository.findAll();
     }
 
-    public Medico findById (String id)
+    public Medico findById (String id){
+        return medicoRepository.findById(id).get();
+    }
+
+    public Medico save (Medico medico){
+        return medicoRepository.save(medico);
+    }
+
+    public void delete(String id){
+        medicoRepository.deleteById(id);
+    }
 }
