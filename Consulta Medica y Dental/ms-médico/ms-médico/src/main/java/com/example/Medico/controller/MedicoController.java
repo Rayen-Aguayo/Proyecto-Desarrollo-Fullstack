a064dd2e301a246d77d6b5aa9a9a5c8d16120668
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-05-01 23:26:39 -0400

    feat:update controller

--- Consulta Medica y Dental/ms-médico/ms-médico/src/main/java/com/example/Medico/controller/MedicoController.java
+++ Consulta Medica y Dental/ms-médico/ms-médico/src/main/java/com/example/Medico/controller/MedicoController.java
@@ -3,6 +3,7 @@ package com.example.Medico.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,16 @@ import com.example.Medico.service.MedicoService;
 
 import jakarta.validation.Valid;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
 
 
 
@@ -36,6 +43,43 @@ public class MedicoController {
 
     @PostMapping
     public ResponseEntity<Medico> guardar(@Valid @RequestBody Medico medico) {
-        Medico medico 
+        Medico medicoNuevo = medicoService.save(medico);
+        return ResponseEntity.status(HttpStatus.CREATED).body(medicoNuevo);
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<Medico> buscar(@PathVariable String id) {
+        try {
+            Medico medico = medicoService.findById(id);
+            return ResponseEntity.ok(medico);
+        } catch(Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Medico> actualizar(@PathVariable String id, @Valid @RequestBody Medico medico) {
+        try {
+            Medico med = medicoService.findById(id);
+            med.setNombre(id);
+            med.setRun(medico.getRun());
+            med.setEdad(medico.getEdad());
+            med.setTelefono(medico.getTelefono());
+            med.setEspecialidad(medico.getEspecialidad());
+            medicoService.save(med);
+            return ResponseEntity.ok(medico);
+        } catch(Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable String id) {
+        try {
+            medicoService.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch(Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
