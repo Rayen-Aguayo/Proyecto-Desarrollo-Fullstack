bc7b1f2de2a7a766db35e9efeb38c368f7bffb4c
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-05-02 16:52:55 -0400

    feat: update model y repository

--- Consulta Medica y Dental/ms-ficha médica/Ficha médica/src/main/java/com/example/Ficha/medica/model/FichaMedica.java
+++ Consulta Medica y Dental/ms-ficha médica/Ficha médica/src/main/java/com/example/Ficha/medica/model/FichaMedica.java
@@ -3,6 +3,7 @@ package com.example.Ficha.medica.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +15,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class FichaMedica {
     @Id
+    @NotBlank(message = "El nombre no puede estar vacio")
     private String nombrePaciente;
+    @NotBlank(message = "El run no puede estar vacio")
     private Integer run;
+    @NotBlank(message = "El nombre del medico es obligatorio")
     private String medico;
     private String procedimiento;
     private String medicamentoQueToma;
