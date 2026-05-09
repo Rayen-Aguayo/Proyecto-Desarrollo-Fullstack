75f7811abb6239b426f7e60489fe9e0e787c1d61
javiera-faundez <ja.faundezb@duocuc.cl>
2026-05-08 20:30:19 -0400

    feat: update model pagos

---
+++ Consulta Medica y Dental/ms-pagos/Pagos/src/main/java/com/example/Pagos/PagosRepository/PagosRepository.java
@@ -0,0 +1,9 @@
+package com.example.Pagos.PagosRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagosRepository extends JpaRepository {
+
+}
