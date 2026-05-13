29299e3f86b3ac9cff2cca6bb9fff81579f4fa77
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-05-12 20:45:42 -0400

    feat: update dto

---
+++ Consulta Medica y Dental/ms-ficha médica/Ficha médica/src/main/java/com/example/Ficha/medica/dto/PacienteResponse.java
@@ -0,0 +1,17 @@
+package com.example.Ficha.medica.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PacienteResponse {
+    private String run;
+    private String nombrePaciente;
+    private String alergias;
+    private String enfermedad;
+    private String queMedicamentoEstaTomando;
+
+}
--- Consulta Medica y Dental/ms-ficha médica/Ficha médica/src/main/java/com/example/Ficha/medica/service/FichaMedicaService.java
+++ Consulta Medica y Dental/ms-ficha médica/Ficha médica/src/main/java/com/example/Ficha/medica/service/FichaMedicaService.java
@@ -3,6 +3,8 @@ package com.example.Ficha.medica.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.Ficha.medica.dto.FichaMedicaDTO;
+import com.example.Ficha.medica.model.FichaMedica;
 import com.example.Ficha.medica.repository.FichaMedicaRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,5 +17,7 @@ public class FichaMedicaService {
 
     private final FichaMedicaRepository fichaMedicaRepository;
 
-    public FichaMedica crear()
+    public FichaMedica crear(FichaMedicaDTO dto) {
+        log.info("Crear FichaMedica", keyValue(""));
+    }
 }
