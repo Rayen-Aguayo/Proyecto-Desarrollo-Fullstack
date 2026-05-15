1c8d4e87343e32138fff8b955ecfa779b9c29706
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-05-15 02:04:44 -0400

    feat: update dto y client

---
+++ Consulta Medica y Dental/ms-opinion del Paciente/Experiencia-del-Paciente/src/main/java/com/example/Experiencia/del/Paciente/dto/MedicoResponse.java
@@ -0,0 +1,8 @@
+package com.example.Experiencia.del.Paciente.dto;
+
+import lombok.Data;
+
+@Data
+public class MedicoResponse {
+        private String nombreMedico;
+}
---
+++ Consulta Medica y Dental/ms-opinion del Paciente/Experiencia-del-Paciente/src/main/java/com/example/Experiencia/del/Paciente/dto/PacienteResponse.java
@@ -0,0 +1,5 @@
+package com.example.Experiencia.del.Paciente.dto;
+
+public class PacienteResponse {
+
+}
