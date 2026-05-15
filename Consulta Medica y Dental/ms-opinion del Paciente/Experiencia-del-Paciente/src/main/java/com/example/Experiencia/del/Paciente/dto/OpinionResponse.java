34eb71af77fbb0559eacdd6777f34ae50cf501d6
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-05-15 02:12:37 -0400

    feat: update dto y client

---
+++ Consulta Medica y Dental/ms-opinion del Paciente/Experiencia-del-Paciente/src/main/java/com/example/Experiencia/del/Paciente/dto/OpinionResponse.java
@@ -0,0 +1,21 @@
+package com.example.Experiencia.del.Paciente.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OpinionResponse {
+    private Long id;
+    private String runPaciente;
+    private String nombreMedico;
+    private Integer atencionMedico;
+    private String expliqueSuPuntuacion;
+    private String explicacionTratamiento;
+    private String comentarioMejora;
+    private Integer puntuacionMedico;
+}
