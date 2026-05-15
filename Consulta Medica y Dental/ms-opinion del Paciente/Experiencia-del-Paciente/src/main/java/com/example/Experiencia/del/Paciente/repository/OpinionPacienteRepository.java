6da1923a892f9092d5fcd7f5b69a328b9785280e
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-05-15 00:29:08 -0400

    feat: update repository

---
+++ Consulta Medica y Dental/ms-opinion del Paciente/Experiencia-del-Paciente/src/main/java/com/example/Experiencia/del/Paciente/repository/OpinionPacienteRepository.java
@@ -0,0 +1,9 @@
+package com.example.Experiencia.del.Paciente.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Experiencia.del.Paciente.model.OpinionPaciente;
+
+public interface OpinionPacienteRepository extends JpaRepository<OpinionPaciente, Long>{
+
+}
