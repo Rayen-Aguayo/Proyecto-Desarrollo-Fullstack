8452aa985311e8e5a93125e60a6ccaafe81a41ed
Rayen-Aguayo <ray.aguayo@duocuc.cl>
2026-06-08 15:38:31 -0400

    feat: update config

--- Consulta Medica y Dental/ms-auth/ms-auth/src/main/java/com/example/ms_auth/config/SecurityConfig.java
+++ Consulta Medica y Dental/ms-auth/ms-auth/src/main/java/com/example/ms_auth/config/SecurityConfig.java
@@ -8,25 +8,24 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
             .csrf(csrf -> csrf.disable())
-            .sessionManagement(session ->
+            .sessionManagement(session -> 
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(
-                            "/auth/**",
-                            "/swagger-ui/**",
-                            "/swagger-ui.html",
-                            "/v3/api-docs/**"
-                    ).permitAll()
-                    .anyRequest().authenticated()
+                .requestMatchers(
+                    "/auth/**",
+                    "/swagger-ui/**",
+                    "/swagger-ui.html",
+                    "/v3/api-docs/**"
+                ).permitAll()
+                .anyRequest().authenticated()
             );
 
         return http.build();
     }
 }
-
